2da56ee47a3a0850cb767c84bff4c97e31f517ea
yulongtan <swifties@uw.edu>
2016-06-02 10:32:03 -0700

    added option for all cities

--- services/cities.php
+++ services/cities.php
@@ -1,23 +1,45 @@
-<?php 
-	$target_city = strToLower($_GET["city"]);
+<?php
 	$cities = "../data/cities.txt";
 	$cities_list = explode("\n", file_get_contents($cities));
-	foreach($cities_list as $city) {
-		$parts = explode("|", $city);
-		#print var_dump($parts);
-		if (strToLower($parts[0]) == $target_city) {
-			#list ($target_city, $state, $nickname, $landmark, $coast, $desc) = $city;
-			$data = array(
-				"city" => strtoupper(substr($parts[0], 0, 1)) . substr($parts[0], 1),
-				"state" => $parts[1],
-				"nickname" => $parts[2],
-				"image" => $parts[3],
-				"coast" => $parts[4],
-				"description" => $parts[5],
-			);
-			#header("Content-type: application/json");
-			print json_encode($data);
-			die();
+	if (isset($_GET["city"])) {
+		$target_city = strToLower($_GET["city"]);
+		foreach($cities_list as $city) {
+			$city_info = explode("|", $city);
+			if (strToLower($city_info[0]) == $target_city) {
+				$info = array(
+					"state" => $city_info[1],
+					"nickname" => $city_info[2],
+					"image" => $city_info[3],
+					"coast" => $city_info[4],
+					"description" => $city_info[5],
+				);
+				$data = array(
+					"city" => strtoupper(substr($city_info[0], 0, 1)) . substr($city_info[0], 1),
+					"info" => $info,
+				);
+				#header("Content-type: application/json");
+				print json_encode($data);
+				die();
+			}
 		}
+	} else {
+		$city_array = array();
+		foreach($cities_list as $city) {
+			$city_info = explode("|", $city);
+			$info = array(
+				"state" => $city_info[1],
+				"nickname" => $city_info[2],
+				"image" => $city_info[3],
+				"coast" => $city_info[4],
+				"description" => $city_info[5],
+			);
+			$data = array(
+				"city" => strtoupper(substr($city_info[0], 0, 1)) . substr($city_info[0], 1),
+				"info" => $info,
+			);
+			$city_array[] = array("cities" => $data);
+		}
+		header("Content-type: application/json");
+		print json_encode($data);
 	}
 ?>
